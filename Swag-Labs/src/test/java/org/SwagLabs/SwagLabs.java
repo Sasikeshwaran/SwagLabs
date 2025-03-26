eea7a3d6bc6892254c83ecb6f4ce641cffc28fd1
Sasikeshwaran <98255438+Sasikeshwaran@users.noreply.github.com>
2025-03-26 17:25:37 +0530

    loginPage Testing

---
+++ Swag-Labs/src/test/java/org/SwagLabs/SwagLabs.java
@@ -0,0 +1,27 @@
+package org.SwagLabs;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SwagLabs {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		//LoginPage
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		String text = driver.findElement(By.xpath("//div[@class='product_label']")).getText();
+		System.out.println("Applicatio move this page -> "+text);
+		driver.quit();
+	}
+
+}
